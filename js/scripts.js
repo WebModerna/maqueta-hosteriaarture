6b3d1f7fa27831663c544c08c5f369bada43189f
WebModerna <info@webmoderna.com.ar>
2014-10-20 16:50:44 -0300

    Animación de ancla en las tarifas
    No funciona otra que no sea el index. Veré como lo arreglo.

--- js/scripts.js
+++ js/scripts.js
@@ -107,3 +107,19 @@ if (i.type=="text"){
 	//activación y configuración por defecto
 	$(function(){$(".datepicker").datepicker();});
 };}());
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1500);
+        return false;
+      }
+    }
+  });
+});
